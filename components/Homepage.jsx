9c37c95d294858fadee2f1937bea1312b89a92f3
Taner Hacıoğlu <tanerfb1999@gmail.com>
2023-12-03 21:51:13 +0300

    min height changed again

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -113,7 +113,7 @@ export default function Homepage({ data }) {
         <main className="dark text-foreground bg-background flex min-h-[calc(100dvh)] flex-col items-center justify-between ">
           <Gradient
             className={
-              "w-screen h-screen flex flex-col justify-center items-center p-8 text-center gap-8"
+              "w-screen h-[calc(100dvh)] flex flex-col justify-center items-center p-8 text-center gap-8"
             }
           >
             <h1 className="text-5xl mb-4 text-transparent leading-tight font-semibold bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500">
