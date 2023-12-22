0ebd62ee04976d218682d1ed219000c7d6383eab
Taner Hacıoğlu <tanerfb1999@gmail.com>
2023-12-22 13:04:24 +0300

    added copy url button

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -3,11 +3,20 @@ import Gradient from "@/components/Gradient";
 
 import React, { useCallback, useRef, useEffect } from "react";
 import ReactCanvasConfetti from "react-canvas-confetti";
-import { Input, NextUIProvider, Button, Card } from "@nextui-org/react";
+import {
+  Input,
+  NextUIProvider,
+  Button,
+  Card,
+  Tooltip,
+} from "@nextui-org/react";
 import moment from "moment/moment";
 import Result from "./Result";
 import { useMediaQuery } from "react-responsive";
 import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { LinkIcon } from "../app/link.svg";
 
 const canvasStyles = {
   position: "fixed",
@@ -38,19 +47,33 @@ function isDateValid(slug) {
 export default function Homepage({ data }) {
   const searchParams = useSearchParams();
   const slug = searchParams.get("q");
-
   const [date, setDate] = React.useState(isDateValid(slug) ? slug : "");
   const [isFound, setIsFound] = React.useState(false);
+  const [isTooltipOpen, setIsTooltipOpen] = React.useState(false);
   const [planet, setPlanet] = React.useState({});
   const [dayDiff, setDayDiff] = React.useState(null);
   const [lastFoundFor, setLastFoundFor] = React.useState("");
   const [invalidInput, setInvalidInput] = React.useState(false);
+
+  const router = useRouter();
+
+  const createQueryString = (name, value) => {
+    const params = new URLSearchParams();
+    params.set(name, value);
+
+    return params.toString();
+  };
 
   const isBigScreen = useMediaQuery({ query: "(min-width: 800px)" });
 
   const isDateChanged = date !== lastFoundFor;
 
   const refAnimationInstance = useRef(null);
+
+  function handleCopyUrl() {
+    navigator.clipboard.writeText(window.location.href);
+    setIsTooltipOpen(true);
+  }
 
   const getInstance = useCallback((instance) => {
     refAnimationInstance.current = instance;
@@ -105,6 +128,7 @@ export default function Homepage({ data }) {
   function handleClick() {
     if (!validateInput()) return;
     if ((date && !isFound) || (date && isDateChanged)) {
+      router.push("/" + "?" + createQueryString("q", date));
       const diffInDays = moment().diff(moment(date), "days");
       const planet = findBirthday(data, diffInDays);
       fire();
@@ -172,14 +196,41 @@ export default function Homepage({ data }) {
               refConfetti={getInstance}
               style={canvasStyles}
             />
-            <Button
-              color="primary"
-              variant="shadow"
-              className="w-[300px]"
-              onClick={handleClick}
-            >
-              Calculate
-            </Button>
+            <div style={{ width: "300px", display: "flex", gap: "16px" }}>
+              <Button
+                color="primary"
+                variant="shadow"
+                className="w-[300px]"
+                onClick={handleClick}
+              >
+                Calculate
+              </Button>
+              {isFound && (
+                <Tooltip
+                  content="The link is copied to your clipboard."
+                  onOpenChange={(open) => setIsTooltipOpen(false)}
+                  closeDelay={1000}
+                  isOpen={isTooltipOpen}
+                  color="foreground"
+                  placement="bottom"
+                  offset={15}
+                >
+                  <Button isIconOnly onClick={handleCopyUrl}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="16"
+                      width="20"
+                      viewBox="0 0 640 512"
+                    >
+                      <path
+                        fill="white"
+                        d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z"
+                      />
+                    </svg>
+                  </Button>
+                </Tooltip>
+              )}
+            </div>
           </Gradient>
         </main>
       </div>
